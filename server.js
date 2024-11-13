718b029f32f419823ec0147960a97501f488ab66
Eleftherios Kalampakas <leeburton01@gmail.com>
2024-11-13 01:10:22 +0100

    backend new cors updates

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-// Import Routes //
+// Import Routes
 const venueRoutes = require("./routes/venues");
 const worksRoutes = require("./routes/works");
 const authRoutes = require("./routes/auth");
 
 const app = express();
 
-// Enhanced Logging Middleware //
+// Enhanced Logging Middleware
 app.use((req, res, next) => {
   console.log(
     `Incoming request: ${req.method} ${req.url} - Headers:`,
@@ -25,9 +25,9 @@ app.use((req, res, next) => {
 // Serve static files from "uploads" folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Middleware //
-app.use(express.json({ limit: "150mb" })); // Set JSON limit to 150MB
-app.use(express.urlencoded({ limit: "150mb", extended: true })); // Set URL-encoded limit to 150MB
+// Middleware
+app.use(express.json({ limit: "150mb" }));
+app.use(express.urlencoded({ limit: "150mb", extended: true }));
 
 // CORS Configuration
 const corsOptions = {
@@ -35,12 +35,16 @@ const corsOptions = {
     "https://project303.netlify.app", // Deployed frontend URL
     "http://localhost:5173", // Local development URL
   ],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Allow credentials (e.g., cookies, tokens)
 };
 app.use(cors(corsOptions));
 
-// MongoDB Connection //
+// Handle CORS Preflight Requests
+app.options("*", cors(corsOptions));
+
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -49,17 +53,17 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.log("Error connecting to MongoDB:", error));
 
-// Routes //
+// Routes
 app.use("/venues", venueRoutes);
 app.use("/works", worksRoutes);
 app.use("/auth", authRoutes);
 
-// Basic route //
+// Basic route
 app.get("/", (req, res) => {
   res.send("Server is up and running!");
 });
 
-// Start Server //
+// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
